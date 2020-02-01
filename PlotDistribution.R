7b609eb3f93fc54b8a22707fc9a07ef38e652420
Ryan Thomas <rthomas2003@gmail.com>
2020-02-01 11:42:08 -0800

    Added dynamic param elements

--- PlotDistribution.R
+++ PlotDistribution.R
@@ -10,13 +10,31 @@ ui <- tagList(
                           "Normal" = "norm",
                           "Lognormal" = "lnorm",
                           "Expontential" = "exp")),
+  uiOutput("params"),
   colourInput("color", 
               label = "What color should it be?", 
+              value = "black",
               showColour = "both"),
   plotOutput("density")
 )
 
 server <- function(input, output, session) {
+    output$params <- renderUI({
+        req(input$dist)
+        dfun <- get(paste0("d", input$dist))
+        params <- names(formals(dfun))
+        use_params <- params[!(params %in% c("x", "log"))]
+        numInputs <- list()
+        for(i in seq_along(use_params)) {
+            numInputs[[i]] <- numericInput(
+                paste0("param_", use_params[i]), 
+                label = paste("Enter ", use_params[i], ": "), 
+                value = 1
+            )
+        }
+        class(numInputs) <- c("shiny.tag.list", "list")
+        numInputs
+    })
     output$density <- renderPlot({
         req(input$dist)
         dfun <- get(paste0("d", input$dist))
@@ -27,7 +45,8 @@ server <- function(input, output, session) {
         color <- input$color
         dat <- tibble(vals = vals, probs = probs)
         ggplot(dat, aes(x = vals, y = probs)) + geom_line(color = color)
-    })  
+    }) 
+    options(shiny.reactlog = TRUE)
 }
 
 shinyApp(ui, server)
